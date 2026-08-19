0ebf702b2c01418c6e9e3a3106a5a9ac13cf0ea7
Your Name <20379@charansanitwong.ac.th>
2026-08-19 15:36:38 +0700

    test11

--- scirpt.js
+++
@@ -1,4 +0,0 @@
-function changeText() {
-    // สั่งให้ h1 เปลี่ยนข้อความเป็นอย่างอื่น
-    document.getElementById("title").innerText = "ยินดีด้วย คุณเขียนโค้ดสำเร็จแล้ว!";
-}
---
+++ script.js
@@ -0,0 +1,4 @@
+function changeText() {
+    // สั่งให้ h1 เปลี่ยนข้อความเป็นอย่างอื่น
+    document.getElementById("title").innerText = "ยินดีด้วย คุณเขียนโค้ดสำเร็จแล้วหรอ!";
+}
